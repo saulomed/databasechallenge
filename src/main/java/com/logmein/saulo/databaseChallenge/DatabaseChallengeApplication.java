f859f5756e27ae3d3ded9a008d66e5874b72bd37
Saulo Santos <saulocs@ciandt.com>
2021-11-06 22:52:38 -0300

    config swagger

--- src/main/java/com/logmein/saulo/databaseChallenge/DatabaseChallengeApplication.java
+++ src/main/java/com/logmein/saulo/databaseChallenge/DatabaseChallengeApplication.java
@@ -3,6 +3,7 @@ package com.logmein.saulo.databaseChallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -17,6 +18,12 @@ public class DatabaseChallengeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseChallengeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(DatabaseChallengeApplication.class);
 	}
 
 }

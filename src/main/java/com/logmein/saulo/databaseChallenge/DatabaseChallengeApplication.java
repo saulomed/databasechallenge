072dead9331e1c13e84b2a7254433e716710ae50
Saulo Santos <saulocs@ciandt.com>
2021-11-06 22:49:12 -0300

    config swagger

--- src/main/java/com/logmein/saulo/databaseChallenge/DatabaseChallengeApplication.java
+++ src/main/java/com/logmein/saulo/databaseChallenge/DatabaseChallengeApplication.java
@@ -3,6 +3,7 @@ package com.logmein.saulo.databaseChallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -12,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(
 		basePackages = {"com.logmein.saulo.databaseChallenge.server.repository"}
 )
-public class DatabaseChallengeApplication {
+public class DatabaseChallengeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseChallengeApplication.class, args);
--- src/main/java/com/logmein/saulo/databaseChallenge/server/resource/KeyValueResource.java
+++ src/main/java/com/logmein/saulo/databaseChallenge/server/resource/KeyValueResource.java
@@ -7,8 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/v1/keyvalue")
